802c65c031ae60529d6eeb5a2b6a75d2189a9abe
ERAZER\dmsr <dominik_messerschmidt@yahoo.de>
2018-02-20 21:11:15 +0100

    Adapted build scripts

--- FileCopyR/BuildAndRun.bat
+++ FileCopyR/BuildAndRun.bat
@@ -1,7 +1,7 @@
 set "APP_NAME=FileCopyR"
 
-%JAVA_HOME%\javac -d bin src\main\*
+"%JAVA_HOME%\javac" -d bin src\main\*
 cd bin
-%JAVA_HOME%\jar cmf MANIFEST.MF ..\%APP_NAME%.jar *
+"%JAVA_HOME%\jar" cmf MANIFEST.MF ..\%APP_NAME%.jar *
 cd ..
-%JAVA_HOME%\java -jar %APP_NAME%.jar %1
+"%JAVA_HOME%\java" -jar %APP_NAME%.jar %1
